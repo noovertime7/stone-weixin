5a0af4d9e780fbdc685c7238e5fe476a6f04eae9
noovertime7 <cc17854330572@163.com>
2024-03-18 20:23:09 +0800

    feat: 登录对接完成

---
+++ src/pages/history/history.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="history">history</div>
+</template>
+
+<script lang="ts" setup>
+  const props = defineProps();
+</script>
+
+<style scoped></style>
---
+++ src/types/home.d.ts
@@ -0,0 +1,55 @@
+/** 首页-广告区域数据类型 */
+export type BannerItem = {
+  /** 跳转链接 */
+  hrefUrl: string
+  /** id */
+  id: string | number
+  /** 图片链接 */
+  imgUrl: string
+  /** 跳转类型 */
+  type: number
+}
+
+/** 首页-前台类目数据类型 */
+export type CategoryItem = {
+  /** 图标路径 */
+  icon: string
+  /** id */
+  id: string
+  /** 分类名称 */
+  name: string
+}
+
+/** 首页-热门推荐数据类型 */
+export type HotItem = {
+  /** 说明 */
+  alt: string
+  /** id */
+  id: string
+  /** 图片集合[ 图片路径 ] */
+  pictures: string[]
+  /** 跳转地址 */
+  target: string
+  /** 标题 */
+  title: string
+  /** 推荐类型 */
+  type: string
+}
+
+/** 猜你喜欢-商品类型 */
+export type GuessItem = {
+  /** 商品描述 */
+  desc: string
+  /** 商品折扣 */
+  discount: number
+  /** id */
+  id: string
+  /** 商品名称 */
+  name: string
+  /** 商品已下单数量 */
+  orderNum: number
+  /** 商品图片 */
+  picture: string
+  /** 商品价格 */
+  price: number
+}
