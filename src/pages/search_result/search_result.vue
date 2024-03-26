12c5a25487a090f7c2a2b24b816c797814fe00f9
noovertime7 <cc17854330572@163.com>
2024-03-26 16:20:54 +0800

    feat: 增加对安装历史的搜索

--- src/pages/search_result/search_result.vue
+++ src/pages/search_result/search_result.vue
@@ -10,17 +10,26 @@
         <view :duration="200" class="flex-1 flex flex-column" @change="swiperChange">
           <view class="flex">
             <scroll-view class="flex-1" scroll-y @scrolltolower="handleLoadMore(tabs[current])">
-              <XtxStonelist
-                @tap="pushStoneDetail(item.id)"
-                tag="one"
-                :stone="item"
-                v-for="(item, index) in tabs[current].list"
-                :key="index"
-              ></XtxStonelist>
+              <view v-for="(item, index) in tabs[current].list" :key="index">
+                <!--           -->
+                <XtxStonelist
+                  v-if="tabs[current].type == 'stone'"
+                  @tap="pushStoneDetail(item.id)"
+                  tag="one"
+                  :stone="item"
+                ></XtxStonelist>
+
+                <XtxRecordList
+                  v-if="tabs[current].type == 'record'"
+                  :record="item"
+                  @tap="pushRecordDetail"
+                >
+                </XtxRecordList>
+              </view>
             </scroll-view>
           </view>
           <view class="loading-text">
-            {{ finish ? '没有更多数据~' : '正在加载...' }}
+            {{ tabs[current].finish ? '没有更多数据~' : '正在加载...' }}
           </view>
         </view>
       </view>
@@ -36,6 +45,8 @@ import XtxTab from '@/components/XtxTab.vue'
 import type { Stone } from '../../types/stone'
 import { pageStone } from '@/services/stone'
 import XtxStonelist from '@/components/XtxStonelist.vue'
+import { pageRecord } from '../../services/record'
+import type { Record } from '../../types/record_d'
 
 // 接收页面参数
 const query = defineProps<{
@@ -54,12 +65,19 @@ const pushStoneDetail = (id: number) => {
   })
 }
 
+const pushRecordDetail = (id: number) => {
+  uni.navigateTo({
+    url: `/pages/record/record?id=${id}`,
+  })
+}
+
 type TabItem = {
   name: string
-  list: Stone[]
+  list: Stone[] | Record[]
   page: number
   pageSize: number
-  type: 'stone' | 'history'
+  type: 'stone' | 'record'
+  finish: boolean
 }
 
 const tabs = ref<TabItem[]>([
@@ -69,18 +87,20 @@ const tabs = ref<TabItem[]>([
     page: 1,
     pageSize: 10,
     type: 'stone',
+    finish: false,
   },
   {
     name: '安装记录',
     list: [],
     page: 1,
     pageSize: 10,
-    type: 'history',
+    type: 'record',
+    finish: false,
   },
 ])
 
 const handleLoadMore = async (t: TabItem) => {
-  if (finish.value) {
+  if (t.finish) {
     return
   }
   t.page++
@@ -103,31 +123,37 @@ const swiperChange = (e: any) => {
   }
 }
 // 已结束标记
-const finish = ref(false)
 const getData = async () => {
   let tab = tabs.value[current.value]
   switch (tab.type) {
     case 'stone': {
-      if (finish.value === true) {
+      if (tab.finish === true) {
         return
       }
       let res = await pageStone(pageParams)
       // 分页条件
       if (res.data.total < pageParams.pageSize) {
-        finish.value = true
+        tab.finish = true
       }
       tab.list = tab.page == 1 ? res.data.list : [...tab.list, ...res.data.list]
 
       break
     }
 
-    case 'history': {
-      console.log('history')
+    case 'record': {
+      if (tab.finish === true) {
+        return
+      }
+      let res = await pageRecord(pageParams)
+      // 分页条件
+      if (res.data.total < pageParams.pageSize) {
+        tab.finish = true
+      }
+      tab.list = tab.page == 1 ? res.data.list : [...tab.list, ...res.data.list]
+
       break
     }
   }
-  console.log('tab = ', tab)
-  console.log('list = ', tab.list)
 }
 
 onShow(() => {
